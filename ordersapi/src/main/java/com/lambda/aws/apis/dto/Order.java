5e6976ef269cdd8c3de67964c0f149abaaa356a2
lelis <lelis685@gmail.com>
2021-05-16 11:12:47 -0300

    first commit

---
+++ ordersapi/src/main/java/com/lambda/aws/apis/dto/Order.java
@@ -0,0 +1,18 @@
+package com.lambda.aws.apis.dto;
+
+public class Order {
+
+	public int id;
+	public String itemName;
+	public int quantity;
+	
+	public Order() {
+	}
+
+	public Order(int id, String itemName, int quantity) {
+		this.id = id;
+		this.itemName = itemName;
+		this.quantity = quantity;
+	}
+	
+}
